e1f049ef498b03bdbf0126fec3516d7cc6c396f7
Soham Shinde <sohamsshinde04@gmail.com>
2023-10-21 12:21:52 +0400

    Added Movement
    Made movement for both the characters. Characters are controlled by W, A, D & left, up, right arrow keys
    
    Added colliders on all the platforms.

---
+++ Assets/Scripts/movement2.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement2 : MonoBehaviour
+{
+    public float jumpHeight = 4.0f;
+    public float jumpDuration = 0.5f;
+    public float movementSpeed = 5.0f;
+
+    private bool isJumping = false;
+    private float jumpStartTime;
+    private float gravityScale;
+
+    [SerializeField] private Rigidbody2D playerRb;
+
+    private void Start()
+    {
+        gravityScale = playerRb.gravityScale;
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal 2");
+
+        Vector2 movementVelocity = new Vector2(horizontalInput * movementSpeed, playerRb.velocity.y);
+        playerRb.velocity = movementVelocity;
+
+        if (Input.GetButtonDown("Vertical") && !isJumping)
+        {
+
+            float initialJumpVelocity = (2 * jumpHeight) / jumpDuration;
+
+            playerRb.velocity = new Vector2(playerRb.velocity.x, initialJumpVelocity);
+            isJumping = true;
+            jumpStartTime = Time.time;
+        }
+
+        if (isJumping)
+        {
+            float timeSinceJumpStart = Time.time - jumpStartTime;
+            if (timeSinceJumpStart >= jumpDuration)
+            {
+                isJumping = false;
+                playerRb.gravityScale = gravityScale;
+            }
+            else
+            {
+                float gravityEffect = (jumpHeight * 2) / Mathf.Pow(jumpDuration, 2);
+                playerRb.velocity = new Vector2(playerRb.velocity.x, playerRb.velocity.y - gravityEffect * Time.deltaTime);
+            }
+        }
+    }
+}
